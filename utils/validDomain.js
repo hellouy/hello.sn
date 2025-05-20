f2a4fa5d71576b99968201d5b1e103131ae50815
hellouy <domain@nic.bn>
2025-05-20 21:08:50 +0800

    Update validDomain.js

--- utils/validDomain.js
+++ utils/validDomain.js
@@ -1,3 +1,4 @@
 module.exports = function validDomain(domain) {
-  return /^([a-zA-Z0-9\u4e00-\u9fa5][a-zA-Z0-9\u4e00-\u9fa5\-\.]{0,62})\.([a-zA-Z0-9\u4e00-\u9fa5\-]{2,})$/.test(domain);
+  // 允许中文、国际化后缀
+  return /^([a-zA-Z0-9\u4e00-\u9fa5][a-zA-Z0-9\u4e00-\u9fa5\-\.]{0,62})\.([a-zA-Z\u4e00-\u9fa5\-]{2,})$/.test(domain);
 };
