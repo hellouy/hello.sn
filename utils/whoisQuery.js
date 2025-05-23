c547402e549f9b7872fe3acbe993d990530063ac
hellouy <domain@nic.bn>
2025-05-23 20:53:35 +0800

    Update whoisQuery.js

--- utils/whoisQuery.js
+++ utils/whoisQuery.js
@@ -22,46 +22,52 @@ function isUnregisteredWhois(whoisRaw) {
   return unregisteredPatterns.some(re => re.test(normalized));
 }
 
-// 兼容各种主流/冷门TLD和ccTLD字段
+// 支持多语言、多分隔符、多变体
+function makeFieldRegexes(fieldNames, multi = false) {
+  // 分隔符可以是 : ： = 空格
+  const sep = '[\\s:：=]+';
+  return fieldNames.map(f => new RegExp(`${f}${sep}([^\r\n]+)`, multi ? 'ig' : 'i'));
+}
+
 function parseSimpleWhois(raw) {
   if (!raw) return {};
-  // 允许冒号/中文冒号/等号/空格
-  const sep = '[\\s:：=]+';
-  // 允许字段名有空格、下划线、中划线、小写/大写
-  const makeFieldRegex = (fields, multi = false) =>
-    fields.map(f =>
-      new RegExp(`${f}${sep}([^\r\n]+)`, multi ? 'ig' : 'i')
-    );
-
   const matchers = {
-    domainName: makeFieldRegex([
+    domainName: makeFieldRegexes([
       "Domain Name", "domain", "域名", "ドメイン名", "Domaine", "Nome do Domínio"
     ]),
-    registrar: makeFieldRegex([
-      "Registrar", "Sponsoring Registrar", "注册商", "Registrar Name", "Registrar/Registration Service Provider"
+    registrar: makeFieldRegexes([
+      "Registrar", "Sponsoring Registrar", "注册商", "Registrar Name", "Registrar/Registration Service Provider", "注册服务机构"
     ]),
-    creationDate: makeFieldRegex([
-      "Creation Date", "Created On", "Registered On", "Registration Time", "Domain Registration Date", "注册时间", "Created", "Domain Create Date", "Created-Date"
+    creationDate: makeFieldRegexes([
+      "Creation Date", "Created On", "Created", "Domain Registration Date",
+      "注册时间", "注册日期", "Registration Date", "Registered On", "Domain Create Date", "Domain Created", "Domain created", "成立时间"
     ]),
-    updatedDate: makeFieldRegex([
-      "Updated Date", "Last Updated On", "Last Update", "Domain Last Updated Date", "更新时间", "Updated", "Last-Modified"
+    updatedDate: makeFieldRegexes([
+      "Updated Date", "Last Updated On", "Last Update", "Domain Last Updated Date", "更新时间", "Updated", "Last-Modified", "Domain Last Modified"
     ]),
-    expiryDate: makeFieldRegex([
-      "Registrar Registration Expiration Date", "Registry Expiry Date", "Expiration Date", "Expires On", "Paid-till", "Expiry Date", "到期时间", "Expire", "Renewal Date", "Domain Expiration Date", "Expiry", "Expires", "Expires-Date"
+    expiryDate: makeFieldRegexes([
+      "Registrar Registration Expiration Date", "Registry Expiry Date", "Expiration Date", "Expires On", "Paid-till",
+      "Expiry Date", "到期时间", "Expire", "Renewal Date", "Domain Expiration Date", "Expiry", "Expires", "Expires-Date", "到期日期"
     ]),
-    status: makeFieldRegex([
+    status: makeFieldRegexes([
       "Status", "Domain Status", "状态", "Domain statuses", "Domain Statuses"
     ], true),
-    nameServers: makeFieldRegex([
+    nameServers: makeFieldRegexes([
       "Name Server", "Nameserver", "Nserver", "DNS", "Name Servers", "DNS servers", "Domain servers", "域名服务器"
     ], true),
-    whoisServer: makeFieldRegex([
+    whoisServer: makeFieldRegexes([
       "WHOIS Server", "Whois Server", "Registrar Whois", "Whois", "WHOIS"
     ]),
-    registrant: makeFieldRegex([
-      "Registrant Name", "Registrant", "注册人", "Holder", "Domain Holder", "Domain Owner"
+    registrant: makeFieldRegexes([
+      "Registrant Name", "Registrant", "注册人", "Holder", "Domain Holder", "Domain Owner", "所有者"
     ]),
-    dnssec: makeFieldRegex([
+    registrantEmail: makeFieldRegexes([
+      "Registrant Email", "注册人邮箱", "Holder Email", "邮箱"
+    ]),
+    registrantCountry: makeFieldRegexes([
+      "Registrant Country", "注册人国家", "Holder Country", "国家"
+    ]),
+    dnssec: makeFieldRegexes([
       "DNSSEC", "Dnssec", "DNS Sec"
     ])
   };
