1a235889ed225b0d18d80b7d001bb454a9a101ba
hellouy <domain@nic.bn>
2025-05-23 10:02:18 +0800

    Update ResultCard.js

--- components/ResultCard.js
+++ components/ResultCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function ResultCard({ data, registered, protocol, rawWhois, rdap, fullResult }) {
-  // whoisParsed 展示
   if (protocol === "whois" && data && Object.keys(data).length > 0) {
     return (
       <div style={{
@@ -14,7 +13,12 @@ export default function ResultCard({ data, registered, protocol, rawWhois, rdap,
         <h3 style={{ fontWeight: 700, fontSize: 22, color: "#2469f7", margin: 0 }}>
           WHOIS信息
         </h3>
-        <div style={{ fontSize: 15, margin: "10px 0 0 0", color: registered ? "#2ba95b" : "#b33", fontWeight: 600 }}>
+        <div style={{
+          fontSize: 15,
+          margin: "10px 0 16px 0",
+          color: registered ? "#2ba95b" : "#b33",
+          fontWeight: 600
+        }}>
           {registered ? "已注册" : "未注册"}
         </div>
         {data.domainName && <div>域名：{data.domainName}</div>}
@@ -22,22 +26,26 @@ export default function ResultCard({ data, registered, protocol, rawWhois, rdap,
         {data.creationDate && <div>注册时间：{data.creationDate}</div>}
         {data.updatedDate && <div>更新时间：{data.updatedDate}</div>}
         {data.expiryDate && <div>到期时间：{data.expiryDate}</div>}
-        {data.status && <div>状态：{data.status}</div>}
+        {data.status && data.status.length > 0 && (
+          <div>状态：{data.status.join("、")}</div>
+        )}
         {data.nameServers && data.nameServers.length > 0 && (
           <div>DNS服务器：{data.nameServers.join("、")}</div>
         )}
-        {data.org && <div>注册组织：{data.org}</div>}
-        {data.country && <div>国家：{data.country}</div>}
         {data.registrant && <div>注册联系人：{data.registrant}</div>}
-        {data.registrarIANAID && <div>注册商IANA ID：{data.registrarIANAID}</div>}
-        {data.registrarAbuseContactEmail && <div>注册商投诉邮箱：{data.registrarAbuseContactEmail}</div>}
-        {data.registrarAbuseContactPhone && <div>注册商投诉电话：{data.registrarAbuseContactPhone}</div>}
+        {data.registrantOrg && <div>注册组织：{data.registrantOrg}</div>}
+        {data.registrantEmail && <div>注册邮箱：{data.registrantEmail}</div>}
+        {data.registrantCountry && <div>国家：{data.registrantCountry}</div>}
+        {data.registrantCity && <div>城市：{data.registrantCity}</div>}
         {data.registrantStreet && <div>注册地址：{data.registrantStreet}</div>}
-        {data.registrantCity && <div>注册城市：{data.registrantCity}</div>}
-        {data.registrantState && <div>注册省/州：{data.registrantState}</div>}
+        {data.registrantState && <div>省/州：{data.registrantState}</div>}
         {data.registrantPostalCode && <div>邮编：{data.registrantPostalCode}</div>}
         {data.registrantPhone && <div>联系电话：{data.registrantPhone}</div>}
         {data.registrantFax && <div>传真：{data.registrantFax}</div>}
+        {data.adminEmail && <div>管理员邮箱：{data.adminEmail}</div>}
+        {data.techEmail && <div>技术邮箱：{data.techEmail}</div>}
+        {data.abuseContactEmail && <div>投诉邮箱：{data.abuseContactEmail}</div>}
+        {data.abuseContactPhone && <div>投诉电话：{data.abuseContactPhone}</div>}
         {data.emails && data.emails.length > 0 && (
           <div>邮箱：{data.emails.join("、")}</div>
         )}
